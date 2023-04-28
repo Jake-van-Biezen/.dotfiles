27c29b4f7c2ec5739569c97daf3602a98e03c562
Jake-van-Biezen <jake1998@hotmail.nl>
2023-04-28 11:24:56 +0200

    Add Black

--- lvim/lua/user/lsp/init.lua
+++ lvim/lua/user/lsp/init.lua
@@ -1,15 +1,20 @@
-reload "user.lsp.languages.rust"
-reload "user.lsp.languages.golang"
-reload "user.lsp.languages.sh"
-reload "user.lsp.languages.js-ts"
+reload("user.lsp.languages.rust")
+reload("user.lsp.languages.golang")
+reload("user.lsp.languages.sh")
+reload("user.lsp.languages.js-ts")
 
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer", "gopls", "bashls", "tsserver" })
+vim.list_extend(
+	lvim.lsp.automatic_configuration.skipped_servers,
+	{ "rust_analyzer", "gopls", "bashls", "tsserver", "terraform", "black" }
+)
 
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-    { command = "stylua",    filetypes = { "lua" } },
-    { command = "goimports", filetypes = { "go" } },
-    { command = "gofumpt",   filetypes = { "go" } },
-    { command = "stylua",    filetypes = { "lua" } },
-    { command = "shfmt",     filetypes = { "sh", "zsh" } },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "goimports", filetypes = { "go" } },
+	{ command = "gofumpt", filetypes = { "go" } },
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "shfmt", filetypes = { "sh", "zsh" } },
+	{ command = "terraform_fmt", filetypes = { "terraform" } },
+	{ command = "black", filetypes = { "python" } },
+})
